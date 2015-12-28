271cd59332e9871c70ac228b11ca914803ac9fc4
santi <sledesm@gmail.com>
2015-12-28 20:01:20 +0100

    Starting to parse slaveConfig

--- ethercatBindings.cc
+++ ethercatBindings.cc
@@ -43,7 +43,7 @@ static ec_master_state_t master_state ;
 
 static ec_domain_t *domain1 = NULL;
 static ec_domain_state_t domain1_state ;
-static bool initialised=false;
+//static bool initialised=false;
 
 /*static ec_slave_config_t *sc_ana_in = NULL;
 static ec_slave_config_state_t sc_ana_in_state = {};
@@ -162,9 +162,136 @@ void start(const FunctionCallbackInfo<Value>& args) {
 
 }
 
+
 void addSlave(const FunctionCallbackInfo<Value>&args){
     Isolate *isolate=Isolate::GetCurrent();
     HandleScope scope(isolate);
+
+    /*
+        Local<Object>options=args[0]->ToObject();
+        offLoopPeriod=options->Get(String::NewFromUtf8(isolate,"offLoopPeriod"))->NumberValue();
+        printf("ethercatRT. LoopPeriodOffset: %d ",offLoopPeriod);
+
+    */
+
+    Local<Object> options=args[0]->ToObject();
+    printf("got options\nb");
+    Local<Object> res;
+    if (!options->IsObject()){
+        res->Set(String::NewFromUtf8(isolate,"result"),String::NewFromUtf8(isolate,"error"));
+        res->Set(String::NewFromUtf8(isolate,"error"),String::NewFromUtf8(isolate,"Options must be an object"));
+        args.GetReturnValue().Set(res);
+        return;
+    }
+//    offLoopPeriod=options->Get(String::NewFromUtf8(isolate,"offLoopPeriod"))->NumberValue();
+    Local<Array>pdos=Local<Array>::Cast(options->Get(String::NewFromUtf8(isolate,"pdos")));
+    if (!options->Get(String::NewFromUtf8(isolate,"pdos"))->IsObject()){
+        res->Set(String::NewFromUtf8(isolate,"result"),String::NewFromUtf8(isolate,"error"));
+        res->Set(String::NewFromUtf8(isolate,"error"),String::NewFromUtf8(isolate,"invalid object syntax. Please include pdos member"));
+        args.GetReturnValue().Set(res);
+        return;
+    }
+    printf("got pdos\n");
+//Local<Array> sessions = Local<Array>::Cast(
+    uint32_t numPdos=pdos->Length();
+    if (!numPdos){
+        res->Set(String::NewFromUtf8(isolate,"result"),String::NewFromUtf8(isolate,"error"));
+        res->Set(String::NewFromUtf8(isolate,"error"),String::NewFromUtf8(isolate,"Please specify pdos"));
+        args.GetReturnValue().Set(res);
+        return;
+    }
+    for (uint32_t i=0; i<numPdos; i++){
+        Local<Object>pdo=pdos->Get(i)->ToObject();
+        if (!pdo->IsObject()){
+            res->Set(String::NewFromUtf8(isolate,"result"),String::NewFromUtf8(isolate,"error"));
+            res->Set(String::NewFromUtf8(isolate,"error"),String::NewFromUtf8(isolate,"Pdo not an object"));
+            args.GetReturnValue().Set(res);
+            return;
+        }
+        int index=pdo->Get(String::NewFromUtf8(isolate,"index"))->NumberValue();
+        unsigned int uIndex=index;
+        printf("pdo %d, index: 0x%X, index: %d\n",i,uIndex,uIndex);
+    }
+
+/*
+Isolate* isolate = Isolate::GetCurrent();
+     HandleScope scope(isolate);
+     Local<Object> obj = Object::New(isolate);
+     obj->Set(String::NewFromUtf8(isolate,"times"),Number::New(isolate,_times));
+     obj->Set(String::NewFromUtf8(isolate,"maxDif"),Number::New(isolate,maxDif));
+     obj->Set(String::NewFromUtf8(isolate,"avgDif"),Number::New(isolate,avgDif));
+     obj->Set(String::NewFromUtf8(isolate,"accumDif"),Number::New(isolate,accumDif));
+     obj->Set(String::NewFromUtf8(isolate,"curTime"),Number::New(isolate,(double)curTime/1000000));
+     obj->Set(String::NewFromUtf8(isolate,"curSecs"),Number::New(isolate,curSecs));
+     args.GetReturnValue().Set(obj);
+
+
+{"pdos":[{"index":"0x6040","value":0,"size":16,"pinName":"offControlWord0"},{"index":"0x607a","value":0,"size":32,"pinName":"targetPosition0"},{"index":"0x6041","value":0,"size":16,"pinName":"statusWord0"},{"index":"0x6064","value":0,"size":32,"pinName":"actualPosition0"}],"rxPdo":{"index":5633,"records":[0,1]},"txPdo":{"index":6657,"records":[2,3]},"syncs":[{"index":2,"dir":"output","value":1,"records":[0,1],"wd":"EC_WD_DISABLE"},{"index":2,"dir":"input","value":1,"records":[2,3],"wd":"EC_WD_DISABLE"}]}
+slaveConfig:{
+    pdos:[
+         {index:0x6040,value:0x0,size:16,pinName:"offControlWord0"},
+         {index:0x607a,value:0x0,size:32,pinName:"targetPosition0"},
+         {index:0x6041,value:0x0,size:16,pinName:"statusWord0"},
+         {index:0x6064,value:0x0,size:32,pinName:"actualPosition0"}
+    ],
+    rxPdo:{
+        index:0x1601,
+        records:[0,1]
+    },
+    txPdo:{
+       index:0x1A01,
+       records:[2,3]
+    },
+    syncs:[
+      {index:2,
+       dir:"output",
+       value:1,
+       records:[0,1],
+       wd:"EC_WD_DISABLE"
+      },
+      {index:2,
+       dir:"input",
+       value:1,
+       records:[2,3],
+       wd:"EC_WD_DISABLE"
+    ]
+}
+
+   ethercat.addSlave({
+        position:[0,i], // donde están en el bus
+        ids:[0x0000017f,0x00000014], // cuál es el id del dispositivo
+        config:{
+                   pdos:[
+                        {index:0x6040,value:0x0,size:16,pinName:"offControlWord0"},
+                        {index:0x607a,value:0x0,size:32,pinName:"targetPosition0"},
+                        {index:0x6041,value:0x0,size:16,pinName:"statusWord0"},
+                        {index:0x6064,value:0x0,size:32,pinName:"actualPosition0"}
+                   ],
+                   rxPdo:{
+                       index:0x1601,
+                       records:[0,1]
+                   },
+                   txPdo:{
+                      index:0x1A01,
+                      records:[2,3]
+                   },
+                   syncs:[
+                     {index:2,
+                      dir:"output",
+                      value:1,
+                      records:[0,1],
+                      wd:"EC_WD_DISABLE"
+                     },
+                     {index:2,
+                      dir:"input",
+                      value:1,
+                      records:[2,3],
+                      wd:"EC_WD_DISABLE"
+                   ]
+               }
+   });
+*/
+
 
 }
 
